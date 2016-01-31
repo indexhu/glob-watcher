891eca7494a34df1e233a76e225eb71efa3098c5
Blaine Bublitz <blaine.bublitz@gmail.com>
2016-01-31 14:44:46 -0700

    async-done for tasks, track running

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 'use strict';
 
 var chokidar = require('chokidar');
-
-function wrapCallback(cb){
-
-  function onChange(evt, path) {
-    cb();
-  }
-
-  return onChange;
-}
+var asyncDone = require('async-done');
 
 function watch(glob, opt, cb) {
   if (typeof opt === 'function') {
@@ -19,22 +11,34 @@ function watch(glob, opt, cb) {
 
   opt = opt || {};
 
-  var fn;
-  if (typeof cb === 'function') {
-    fn = wrapCallback(cb);
-  }
-
   if (opt.ignoreInitial == null) {
     opt.ignoreInitial = true;
   }
 
-  var watcher = chokidar.watch(glob, opt);
-  if (fn) {
-    watcher
-      .on('change', fn)
-      .on('unlink', fn)
-      .on('add', fn);
+  var running = false;
+
+  function runComplete(err) {
+    // TODO: report errors
+    running = false;
   }
+
+  function onChange(path) {
+    if (running) {
+      return;
+    }
+
+    if (typeof cb === 'function') {
+      running = true;
+      asyncDone(cb, runComplete);
+    }
+  }
+
+  var watcher = chokidar.watch(glob, opt);
+
+  watcher
+    .on('change', onChange)
+    .on('unlink', onChange)
+    .on('add', onChange);
 
   return watcher;
 }
